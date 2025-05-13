a3318e5776504d5d2056c0c05535839c214043ef
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-05-13 14:48:08 +0300

    part 1 final fix

--- src/work_api.py
+++ src/work_api.py
@@ -4,22 +4,35 @@ import requests
 
 
 class ApiWork:
-    __slots__ = ('input_employer', 'input_host', 'only_with_vacancies', 'text', 'input_open_vac')
+    __slots__ = (
+        'input_employer',
+        'input_host',
+        'num',
+        'result_employer',
+    )
 
-    def __init__(self):
+    def __init__(self, num, host):
         """Конструктор"""
         self.input_employer = input('Введите название компании: ')
-        self.input_host = 'hh.ru'
+        self.input_host = host
+        self.num = num
+        self.result_employer = None
 
-    def data_employers(self):
+    def data_employers_api(self):
         """Метод api для получения данных о работодателях с сайта hh.ru"""
         # api компаний
         url = f"https://api.{self.input_host}/employers?text={self.input_employer}"
         # Запрос к api компаний
         response_employer = requests.get(url)
-        result_employer = response_employer.json()
-        if result_employer['found'] >= 10:
-            for item_employ in result_employer['items']:
+        self.result_employer = response_employer.json()
+        return self.result_employer
+
+    def load_in_db(self):
+        """Метод для загрузки в базы данных"""
+        if self.result_employer is None:  # Проверка на обновлённую переменную - self.result_employer
+            self.data_employers_api()
+        if self.result_employer['found'] >= self.num:
+            for item_employ in self.result_employer['items']:
                 response_vac = requests.get(item_employ['vacancies_url'])
                 result_vac = response_vac.json()
                 for item_vac in result_vac['items']:
@@ -34,13 +47,17 @@ class ApiWork:
                         password='10121331'
                     ) as conn:
                             with conn.cursor() as cur:
-                                cur.execute("INSERT INTO employers VALUES (%s, %s, %s)",
-                                            (item_employ['id'], item_employ['name'], item_employ['vacancies_url']))
-                                cur.execute("INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s)",
-                                            (item_vac['id'], item_vac['name'], salary_from, salary_to, currency))
+                                cur.execute(
+                                    "INSERT INTO employers VALUES (%s, %s, %s)",
+                                    (item_employ['id'], item_employ['name'], item_employ['vacancies_url'])
+                                )
+                                cur.execute(
+                                    "INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s)",
+                                    (item_vac['id'], item_vac['name'], salary_from, salary_to, currency)
+                                )
 
-class_obj = ApiWork()
-class_obj.data_employers()
+class_obj = ApiWork(10, 'hh.ru')
+class_obj.load_in_db()
 
 # Запросы для sql
 # SELECT *
