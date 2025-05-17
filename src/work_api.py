804ff8db45729b533da4e835f76a445bec9d48d7
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-05-17 07:18:17 +0300

    finally fix module

--- src/work_api.py
+++ src/work_api.py
@@ -42,18 +42,27 @@ class ApiWork:
 
         url = f"https://api.hh.ru/employers"
         # Запрос к api компаний
-        response_employer = requests.get(
-            url,
-            params=
-            {
-                "text": self.input_employer, # Название компании
-                "per_page": self.num # Ограничение на кол-во результатов
-            },
-            timeout=60,
+        try:
+            response_employer = requests.get(
+                url,
+                params=
+                {
+                    "text": self.input_employer, # Название компании
+                    "per_page": self.num # Ограничение на кол-во результатов
+                },
+                timeout=60,
 
-        )
-        self.result_employer = response_employer.json()
-        return self.result_employer
+            )
+            response_employer.raise_for_status()  # Проверяем статус здесь!
+
+            self.result_employer = response_employer.json()
+            if not isinstance(self.result_employer, dict):
+                raise ValueError("Некорректный формат данных от API")
+            self.result_employer = response_employer.json()
+            return self.result_employer
+        except requests.exceptions.HTTPError as e:
+            logging.error(f"Ошибка API: {e}")
+            self.result_employer = None
 
     def __data_employers_api(self):
         """Приватный метод работы с API"""
